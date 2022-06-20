1ce607435cc2cf035ca5481c9995ab362cb13b10
Nixon-orel <nixon123@mail.ru>
2022-06-20 13:56:33 +0300

    rr_106

---
+++ lesson21/rr_106.rb
@@ -0,0 +1,9 @@
+require 'rexml/document'
+file_name = "./rr_106_data/card.xml"
+abort "Sorry, file not found" unless File.exist?(file_name)
+file = File.new(file_name)
+doc = REXML::Document.new(file)
+print doc.root.elements["name"].text.chomp
+print doc.root.elements["contacts"].text.chomp
+puts doc.root.elements["about"].text
+file.close
