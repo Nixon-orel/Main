16a35bd3862fc0416b133b7692d332a248ed6a04
Nixon-orel <nixon123@mail.ru>
2022-07-11 14:05:18 +0300

    rr_114

---
+++ lesson23/rr_114.rb
@@ -0,0 +1,9 @@
+#https://rubyrush.ru/steps/json-01
+require 'json'
+file = File.read("rr_114_data/rr_114.json")
+card = JSON.parse(file)
+
+puts "#{card['first_name']} #{card['second_name']} #{card['last_name']}"
+puts card['phone']
+puts card['email']
+puts card['skills']
