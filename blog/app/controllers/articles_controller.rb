c96387b007783838192e123163f952c3e723b42e
Nixon-orel <nixon123@mail.ru>
2022-09-03 18:43:32 +0300

    rusrail_2

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "nixon", password: "1234", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
